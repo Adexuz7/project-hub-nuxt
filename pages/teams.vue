2e4444bebc7358d4dcc981b1d56ac42b228031af
karim <4bkarim.amaioua@gmail.com>
2021-07-13 16:46:11 +0100

    pruebasnuxt

---
+++ pages/teams.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-row class="teams-text">
+      <v-col></v-col>
+      <v-col></v-col>
+      <v-col></v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    async asyncData({ $axios }) {
+      return {
+        ideas :await $$axios.$post('/teams',{
+          
+        })
+      }
+    } 
+  }
+</script>
+
+<style  scoped>
+
+</style>
