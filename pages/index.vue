f2dd15ce4aec773c488ac737014db7a58d3274cb
samu <samupebo@gmail.com>
2021-07-13 18:05:14 +0100

    CARROUSEL EN CAMINOI SOCIO

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,39 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-carousel
+    cycle
+    :interval="3500"
+    height="400"
+    hide-delimiter-background
+    show-arrows-on-hover
+  >
+    <v-carousel-item v-for="(slide, i) in slides" :key="i" :src="images[i].src">
+      <v-row class="fill-height" align="center" justify="center">
+        <div class="text-h2">{{ slide }}</div>
+      </v-row>
+    </v-carousel-item>
+  </v-carousel>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: [
+        {
+          src: 'https://images4.alphacoders.com/640/thumbbig-640956.webp',
+        },
+        {
+          src: 'https://cdn.discordapp.com/attachments/833476896175423505/864544544296796180/67343.jpg',
+        },
+        {
+          src: 'https://cdn.discordapp.com/attachments/833476896175423505/864544566144008272/Screenshot_from_2021-07-05_15-35-31.png',
+        },
+        {
+          src: 'https://cdn.discordapp.com/attachments/833476896175423505/864544627091439626/Screenshot_from_2021-07-08_17-37-28.png',
+        },
+      ],
+      slides: ['NEW SERIES', 'NEW WALLPAPERS', 'NEW MEETINGS', 'NEW GAMES'],
+    }
+  },
+}
+</script>
